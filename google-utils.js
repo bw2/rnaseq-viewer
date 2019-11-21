f00742a588bbac2cdf67f77ffecc356ef4620e3e
bw2 <weisburd@broadinstitute.org>
2019-11-21 15:44:05 -0500

    switched from yaml to json

--- google-utils.js
+++ google-utils.js
@@ -4,32 +4,48 @@ const RNA_VIEWER_CLIENT_ID = '61200892608-qphtf65o323setqdcfj4hnf601mmetvs.apps.
 const initGoogleClient = () => new Promise(resolve => {
   // init all gapi modules
   gapi.load('client:auth2', () => {
-    gapi.client.load('storage', 'v1', resolve)
+    gapi.client.load('storage', 'v1', () => {
+      gapi.client.init({
+        'clientId': RNA_VIEWER_CLIENT_ID,
+        'scope': 'https://www.googleapis.com/auth/devstorage.read_only',
+        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/storage/v1/rest']
+      })
+
+      resolve()
+    })
   })
 })
 
-
-const getGoogleAccessToken = async () => {
-  // use OAuth2 to get an access token for RNA-seq viewer to access the google storage API on behalf of the user
-
-  gapi.client.init({
-    'clientId': RNA_VIEWER_CLIENT_ID,
-    'scope': 'https://www.googleapis.com/auth/devstorage.read_only',
-    'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/storage/v1/rest']
-  })
+const googleSignIn = async () => {
 
   const authInstance = await gapi.auth2.getAuthInstance()
   if(!authInstance.isSignedIn.get()) {
     await authInstance.signIn()
   }
+}
 
-  return () => {
-    const user = authInstance.currentUser.get()
-    if (!authInstance.isSignedIn.get()) {
-      user.reloadAuthResponse()
-    }
+const getGoogleUserEmail = async () => {
+  const authInstance = await gapi.auth2.getAuthInstance()
+  const profile = authInstance.currentUser.get().getBasicProfile()
 
-    return user.getAuthResponse().access_token
+  return profile.getEmail()
+}
+
+const getGoogleAccessToken = async () => {
+  // use OAuth2 to get an access token for RNA-seq viewer to access the google storage API on behalf of the user
+  const authInstance = await gapi.auth2.getAuthInstance()
+  const user = authInstance.currentUser.get()
+  if (!authInstance.isSignedIn.get()) {
+    user.reloadAuthResponse()
+  }
+
+  return user.getAuthResponse().access_token
+}
+
+const googleSignOut = async () => {
+  const authInstance = await gapi.auth2.getAuthInstance()
+  if(authInstance.isSignedIn.get()) {
+    await authInstance.signOut()
   }
 }
 
