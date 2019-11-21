73427025408c39013d9822650a4303c41732302f
bw2 <weisburd@broadinstitute.org>
2019-11-21 15:43:06 -0500

    switched from yaml to json

--- generate_config_with_all_samples.py
+++ generate_config_with_all_samples.py
@@ -3,14 +3,15 @@ import subprocess
 
 all_wigs = subprocess.check_output("gsutil ls gs://macarthurlab-rnaseq/bigWig/", shell=True, encoding="UTF-8")
 
-config = "[\n"
+print("[")
 for wig in all_wigs.split():
+
     prefix = os.path.basename(wig).replace(".bigWig", "")
-
-    f.write(4*" " + f"{prefix}:\n")
-    f.write(6*" " + f"coverage_bigWig: {wig}\n")
-    f.write(6*" " + f"spliceJunctions_bed: gs://macarthurlab-rnaseq/SJ_out_bed_for_igvjs/{prefix}.SJ.out.bed.gz\n")
-    f.write(6*" " + f"bam: gs://macarthurlab-rnaseq/star/{prefix}.Aligned.sortedByCoord.out.bam\n")
-
-    
-print(files)
+    print(f"""
+    {{
+        label: "{prefix.upper()}",
+        junctions: " gs://macarthurlab-rna]seq/SJ_out_bed_for_igvjs/{prefix}.SJ.out.bed.gz",
+        bam: "gs://macarthurlab-rnaseq/star/{prefix}.Aligned.sortedByCoord.out.bam",
+    }},
+    """)
+print("]")
